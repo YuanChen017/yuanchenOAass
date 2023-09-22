22c348de4846718837ea9f8d41daa6e3a98fe9a7
unknown <yuc017@ucsd.edu>
2023-09-21 20:14:30 -0700

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production";
 const nextConfig = {
+  basePath: isProduction ? "/yuanchenOAass" : "",
   distDir: "dist",
   images: {
     unoptimized: true,
